aeee4e77674d844a68462e3f4d9728c06b1eaaa1
Naresh Kumar Mahanta <nareshkumarmahanta10@gmail.com>
2024-09-12 11:50:39 +0000

    function

---
+++ basic-js/function.js
@@ -0,0 +1,29 @@
+//nonparameterise function
+
+//value pass- parameters
+//value call- arguments
+
+// //non-parametarise function
+// function sum(){
+//     let a = 100;
+//     let b = 30;
+//     let c = a + b;
+//     console.log(c);
+    
+// }
+
+// sum();
+// sum();
+// sum();
+
+
+//parametarise function
+// function sum(x, y){//datatypes pass called in function its the argument
+//     let c = x + y;
+//     console.log(c);
+    
+// }
+
+// sum(2, 3)//its value pass called parameters 
+// sum(10, 20)
+// sum(60,20)
