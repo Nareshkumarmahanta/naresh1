f0e78adab7c178881d0c9bd6ba10c69e2869c130
Naresh Kumar Mahanta <nareshkumarmahanta10@gmail.com>
2024-09-14 12:53:32 +0000

    array

---
+++ basic-js/array.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4,5,6,7,8,9];
+let tal = 0;
+
+for (let i=0;i<arr.length;i++){
+    tal += arr[i];
+}
+
+console.log("tal:" + tal);//output
+
+
+
+
+
---
+++ basic-js/string.js
@@ -0,0 +1,10 @@
+let str = "hello naresh kumar"
+
+// let len = str.length;
+// console.log(len);
+let index = str.indexOf("naresh");
+console.log(index);
+
+let lastind = str.lastIndexOf("naresh");
+console.log(lastind);
+
